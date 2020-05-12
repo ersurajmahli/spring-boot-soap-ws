bda1b17a1dfb124c0ac54d13209d7de6216a5acb
forgyan <info.forgyan@gmail.com>
2020-05-12 12:34:36 +0530

    change

--- spring-boot-soap-ws/src/main/java/com/example/soapws/LoanEligiblityApplication.java
+++ spring-boot-soap-ws/src/main/java/com/example/soapws/LoanEligiblityApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoanEligiblityApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanEligiblityApplication.class, args);
